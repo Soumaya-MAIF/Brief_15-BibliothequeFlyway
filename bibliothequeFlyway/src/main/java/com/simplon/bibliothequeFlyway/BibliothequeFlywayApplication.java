c6197138e3f0eeb89e5828a49a46578e0a0c1e04
Soum <dev.web21@outlook.fr>
2025-01-08 09:51:53 +0100

    initial commit

---
+++ bibliothequeFlyway/src/main/java/com/simplon/bibliothequeFlyway/BibliothequeFlywayApplication.java
@@ -0,0 +1,13 @@
+package com.simplon.bibliothequeFlyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliothequeFlywayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliothequeFlywayApplication.class, args);
+	}
+
+}
---
+++ bibliothequeFlyway/src/test/java/com/simplon/bibliothequeFlyway/BibliothequeFlywayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplon.bibliothequeFlyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliothequeFlywayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
